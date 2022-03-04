453fd179059c94cd5ec86f37037173fe866de85e
Daiki Usami <daiusa3@icloud.com>
2022-03-04 21:25:53 +0900

    get only today's race

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -114,7 +114,7 @@ export const Top = () => {
     authorizedJsonFetcher,
   );
 
-  const { data: raceData } = useFetch("/api/races", jsonFetcher);
+  const { data: raceData } = useFetch(`/api/races?since=${date.startOf('day')}&until=${date.endOf('day')}`, jsonFetcher);
 
   const handleClickChargeButton = useCallback(() => {
     if (chargeDialogRef.current === null) {
