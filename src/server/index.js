84a2835a2183760db2895a5af646073ea0cf61d9
LilyMameoka <suto.hidetakayellston.com>
2022-03-05 15:25:10 +0900

    :zap: edit Cache-Control

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,7 @@ server.addHook("onRequest", async (req, res) => {
 });
 
 server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "private");
+  res.header("Cache-Control", "public, max-age=300, s-maxage=180");
   res.header("Connection", "close");
 });
 
