548c9cec1c586e932b42948e776f43699c23937c
hijiki51 <hibiki0719euph@gmail.com>
2022-03-05 18:50:13 +0900

    Revert "⚡  compress"
    This reverts commit 2bdab74c43a2eba8a350e493b976092ee6e3e8aa.

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -1,20 +1,15 @@
 import { join } from "path";
 
 import fastifyStatic from "fastify-static";
-import fastifyCompress from "fastify-compress";
 
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const spaRoute = async (fastify) => {
-  fastify.register(fastifyCompress);
-  fastify.register(
-    fastifyStatic, 
-    {
-      root: join(__dirname, "public"),
-      wildcard: false,
-    }
-  );
+  fastify.register(fastifyStatic, {
+    root: join(__dirname, "public"),
+    wildcard: false,
+  });
 
   fastify.get("/favicon.ico", () => {
     throw fastify.httpErrors.notFound();
