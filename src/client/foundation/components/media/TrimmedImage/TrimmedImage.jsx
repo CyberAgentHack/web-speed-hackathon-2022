8b3c2a77cf7c5cdc767a9d87968404217178b27d
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-05 16:05:47 +0900

    💢

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -8,44 +8,43 @@ import React, {useEffect, useState, useRef } from "react";
  */
 
 
-//  export const TrimmedImage = ({ height, src, width }) => {
-//   const [dataUrl, setDataUrl] = useState(null);
+ export const TrimmedImage = ({ height, src, width }) => {
+  const [dataUrl, setDataUrl] = useState(null);
 
-//   useEffect(() => {
-//     const img = new Image();
-//     img.src = src;
-//     img.onload = () => {
-//       const canvas = document.createElement("canvas");
-//       canvas.width = width;
-//       canvas.height = height;
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
 
-//       const isWidthSmaller = img.width <= img.height;
-//       const ratio = isWidthSmaller ? width / img.width : height / img.height;
+      const isWidthSmaller = img.width <= img.height;
+      const ratio = isWidthSmaller ? width / img.width : height / img.height;
 
-//       const ctx = canvas.getContext("2d");
-//       ctx.drawImage(
-//         img,
-//         -(img.width * ratio - width) / 2,
-//         -(img.height * ratio - height) / 2,
-//         img.width * ratio,
-//         img.height * ratio,
-//       );
-//       setDataUrl(canvas.toDataURL());
-//     };
-//   }, [height, src, width]);
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(
+        img,
+        -(img.width * ratio - width) / 2,
+        -(img.height * ratio - height) / 2,
+        img.width * ratio,
+        img.height * ratio,
+      );
+      setDataUrl(canvas.toDataURL());
+    };
+  }, [height, src, width]);
 
-//   return <img src={dataUrl} />;
-// };
-/** @type {React.VFC<Props>} */
-export const TrimmedImage = ({ height, src, width }) => {
-  const img = useRef();
-  const [fit, setFit] = useState("")
-  useEffect(()=>{
-    const imgAspect = img.current.naturalWidth / img.current.naturalHeight
-    const trimmedAspect = width/ height;
-    if(trimmedAspect > imgAspect){
-      setFit('contain')
-    }else setFit('cover')
-  },[])
-  return <img ref={img} height={height} src={src} style={{objectFit:fit}} width={width} />;
+  return <img src={dataUrl} />;
 };
+// export const TrimmedImage = ({ height, src, width }) => {
+//   const img = useRef();
+//   const [fit, setFit] = useState("")
+//   useEffect(()=>{
+//     const imgAspect = img.current.naturalWidth / img.current.naturalHeight
+//     const trimmedAspect = width/ height;
+//     if(trimmedAspect > imgAspect){
+//       setFit('contain')
+//     }else setFit('cover')
+//   },[])
+//   return <img ref={img} height={height} src={src} style={{objectFit:fit}} width={width} />;
+// };
