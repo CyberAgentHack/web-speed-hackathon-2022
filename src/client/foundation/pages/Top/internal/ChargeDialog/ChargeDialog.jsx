5639205f9ff267488d67cd1dd98be9dab19f4ee0
progriro <progriro@gmail.com>
2022-03-05 13:00:19 +0900

    remove framer-motion

--- src/client/foundation/pages/Top/internal/ChargeDialog/ChargeDialog.jsx
+++ src/client/foundation/pages/Top/internal/ChargeDialog/ChargeDialog.jsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
 import React, { forwardRef, useCallback, useState } from "react";
+import styled, { keyframes } from "styled-components";
 import zenginCode from "zengin-code";
 
 import { Dialog } from "../../../../components/layouts/Dialog";
@@ -11,6 +11,18 @@ import { Space } from "../../../../styles/variables";
 
 const CANCEL = "cancel";
 const CHARGE = "charge";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+const FadeIn = styled.div`
+  animation: ${fadeIn} 1s linear;
+`;
 
 /**
  * @typedef Props
@@ -97,11 +109,7 @@ export const ChargeDialog = forwardRef(({ onComplete }, ref) => {
               ))}
             </datalist>
 
-            {bank != null && (
-              <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-                銀行名: {bank.name}銀行
-              </motion.div>
-            )}
+            {bank != null && <FadeIn>銀行名: {bank.name}銀行</FadeIn>}
 
             <label>
               支店コード
@@ -121,11 +129,7 @@ export const ChargeDialog = forwardRef(({ onComplete }, ref) => {
                 ))}
             </datalist>
 
-            {branch && (
-              <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-                支店名: {branch.name}
-              </motion.div>
-            )}
+            {branch && <FadeIn>支店名: {branch.name}</FadeIn>}
 
             <label>
               口座番号
