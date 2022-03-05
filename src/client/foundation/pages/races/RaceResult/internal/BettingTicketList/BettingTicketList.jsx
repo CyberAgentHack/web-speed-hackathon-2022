09ec630befc24e3209471d2d9616bff6a401ac6a
shellingford330 <pg.econ4869@gmail.com>
2022-03-06 02:49:49 +0900

    feat: remove vain fontawesome

--- src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
+++ src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
@@ -60,7 +60,17 @@ export const BettingTicketList = ({ children }) => {
   if (React.Children.count(children) === 0) {
     return (
       <Placeholder>
-        <i className="fas fa-ticket-alt" />
+        <svg
+          style={{
+            fill: "currentColor",
+            height: "22.5px",
+            width: "20px",
+          }}
+          viewBox="0 0 576 512"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M0 128C0 92.65 28.65 64 64 64H512C547.3 64 576 92.65 576 128V208C549.5 208 528 229.5 528 256C528 282.5 549.5 304 576 304V384C576 419.3 547.3 448 512 448H64C28.65 448 0 419.3 0 384V304C26.51 304 48 282.5 48 256C48 229.5 26.51 208 0 208V128z" />
+        </svg>
         <div>購入した拳券はありません</div>
       </Placeholder>
     );
--- src/client/foundation/side-effects.js
+++
@@ -1,7 +0,0 @@
-import { dom, library } from "@fortawesome/fontawesome-svg-core";
-import { faHandPeace } from "@fortawesome/free-regular-svg-icons";
-import { faInfoCircle, faTicketAlt } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faInfoCircle, faTicketAlt, faHandPeace);
-
-dom.watch();
