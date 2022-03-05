1591a8a86d7270a5585a61be2f98ec01191e13b5
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-03-06 00:04:45 +0900

    add fastify opts http2

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -9,7 +9,11 @@ import { initialize } from "../typeorm/initialize.js";
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
-export const apiRoute = async (fastify) => {
+export const apiRoute = async (fastif, optsy) => {
+  module.exports.options = {
+    http2: true,
+    logger: true,
+  };
   fastify.get("/users/me", async (req, res) => {
     const repo = (await createConnection()).getRepository(User);
 
