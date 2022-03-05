efacb4e4abbffa09f8020c82aa5b3d3f0c063a32
shoya ijichi <e1q19006@oit.ac.jp>
2022-03-05 12:14:58 +0900

    change image clip method

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -9,30 +9,30 @@ import React, { useEffect, useState } from "react";
 
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
-  const [dataUrl, setDataUrl] = useState(null);
+  // const [dataUrl, setDataUrl] = useState(null);
 
-  useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
+  // useEffect(() => {
+  //   const img = new Image();
+  //   img.src = src;
+  //   img.onload = () => {
+  //     const canvas = document.createElement("canvas");
+  //     canvas.width = width;
+  //     canvas.height = height;
 
-      const isWidthSmaller = img.width <= img.height;
-      const ratio = isWidthSmaller ? width / img.width : height / img.height;
+  //     const isWidthSmaller = img.width <= img.height;
+  //     const ratio = isWidthSmaller ? width / img.width : height / img.height;
 
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(
-        img,
-        -(img.width * ratio - width) / 2,
-        -(img.height * ratio - height) / 2,
-        img.width * ratio,
-        img.height * ratio,
-      );
-      setDataUrl(canvas.toDataURL());
-    };
-  }, [height, src, width]);
+  //     const ctx = canvas.getContext("2d");
+  //     ctx.drawImage(
+  //       img,
+  //       -(img.width * ratio - width) / 2,
+  //       -(img.height * ratio - height) / 2,
+  //       img.width * ratio,
+  //       img.height * ratio,
+  //     );
+  //     setDataUrl(canvas.toDataURL());
+  //   };
+  // }, [height, src, width]);
 
-  return <img src={dataUrl} />;
+  return <img src={src} width={width} height={height} style={{objectFit:'cover'}} />;
 };
