6babd728b3d15f5d12488dd30ab0f72f095c51ed
trasta <trasta298@gmail.com>
2022-03-06 05:57:38 +0900

    ⚡️ fontサイズを小さく

--- src/client/foundation/styles/GlobalStyle.js
+++ src/client/foundation/styles/GlobalStyle.js
@@ -27,6 +27,6 @@ export const GlobalStyle = createGlobalStyle`
     font-family: "Senobi-Gothic";
     font-weight: bold;
     font-display: swap;
-    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.ttf") format("truetype");
+    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold-Num.ttf") format("truetype");
   }
 `;
