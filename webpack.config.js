a668f7fc5922f4627e0d8324b7d1af89536c2acf
trasta <trasta298@gmail.com>
2022-03-05 13:31:15 +0900

    ➕ add css-minimizer-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const nodeExternals = require("webpack-node-externals");
 
@@ -51,6 +52,13 @@ module.exports = [
       ],
     },
     name: "client",
+    optimization: {
+      minimize: true,
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin(),
+      ],
+    },
     output: {
       path: DIST_PUBLIC,
     },
