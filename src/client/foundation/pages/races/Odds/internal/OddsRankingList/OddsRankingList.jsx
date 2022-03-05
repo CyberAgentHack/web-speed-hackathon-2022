24260f87f5f8f9ca0a19fde474ae0a20eaa48104
trasta <trasta298@gmail.com>
2022-03-05 12:57:04 +0900

    🔥 remove sort lodash

--- src/client/foundation/pages/races/Odds/internal/OddsRankingList/OddsRankingList.jsx
+++ src/client/foundation/pages/races/Odds/internal/OddsRankingList/OddsRankingList.jsx
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import React from "react";
 import styled from "styled-components";
 
@@ -66,10 +65,8 @@ const RankNo = styled.div`
 
 /** @type {React.VFC<Props>} */
 export const OddsRankingList = ({ isRaceClosed, odds, onClickOdds }) => {
-  const sortedOdds = _.take(
-    _.sortBy(odds, (item) => item.odds),
-    50,
-  );
+  // oddsでソートした後に前から50個とってくる
+  const sortedOdds = odds.sort((a, b) => a.odds - b.odds).slice(0, 50);
 
   return (
     <Wrapper>
