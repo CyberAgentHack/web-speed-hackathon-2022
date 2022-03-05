38295f932bb4d7717635b215fd25ed2655e4ccc1
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-05 23:42:58 +0900

    CLS対策

--- src/client/foundation/routes.jsx
+++ src/client/foundation/routes.jsx
@@ -5,8 +5,8 @@ import { CommonLayout } from "./layouts/CommonLayout";
 
 const Top = lazy(()=>import('./pages/Top/Top'))
 const Odds = lazy(()=>import('./pages/races/Odds/Odds'))
-const RaceCard = lazy(()=>import('./pages/races/RaceCard'))
-const RaceResult = lazy(()=>import('./pages/races/RaceResult'))
+const RaceCard = lazy(()=>import('./pages/races/RaceCard/RaceCard'))
+const RaceResult = lazy(()=>import('./pages/races/RaceResult/RaceResult'))
 
 /** @type {React.VFC} */
 export const Routes = () => {
@@ -14,12 +14,12 @@ export const Routes = () => {
     <RouterRoutes>
       <Route element={<CommonLayout />} path="/">
         
-          <Route index element={<Suspense fallback={<div>loading...</div>}><Top /></Suspense>} />
-          <Route element={<Suspense fallback={<div>loading...</div>}><Top /></Suspense>} path=":date" />
+          <Route index element={<Suspense fallback={<div style={{height:"100vh"}}>loading...</div>}><Top /></Suspense>} />
+          <Route element={<Suspense fallback={<div style={{height:"100vh"}}>loading...</div>}><Top /></Suspense>} path=":date" />
           <Route path="races/:raceId">
-            <Route element={<Suspense fallback={<div>loading...</div>}><RaceCard /></Suspense>} path="race-card" />
-            <Route element={<Suspense fallback={<div>loading...</div>}><Odds /></Suspense>} path="odds" />
-            <Route element={<Suspense fallback={<div>loading...</div>}><RaceResult /></Suspense>} path="result" />
+            <Route element={<Suspense fallback={<div style={{height:"100vh"}}>loading...</div>}><RaceCard /></Suspense>} path="race-card" />
+            <Route element={<Suspense fallback={<div style={{height:"100vh"}}>loading...</div>}><Odds /></Suspense>} path="odds" />
+            <Route element={<Suspense fallback={<div style={{height:"100vh"}}>loading...</div>}><RaceResult /></Suspense>} path="result" />
           </Route>
       </Route>
     </RouterRoutes>
