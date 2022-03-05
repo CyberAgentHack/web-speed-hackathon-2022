23ba9b9a01811df42c426b1ddffdb578122831b8
shellingford330 <pg.econ4869@gmail.com>
2022-03-05 15:33:58 +0900

    feat: text compression

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -1,11 +1,14 @@
 import { join } from "path";
 
+import fastifyCompress from "fastify-compress";
 import fastifyStatic from "fastify-static";
 
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const spaRoute = async (fastify) => {
+  fastify.register(fastifyCompress);
+
   fastify.register(fastifyStatic, {
     root: join(__dirname, "public"),
     wildcard: false,
