55113bb91028d56950bab681fe84bb95281d030d
shellingford330 <pg.econ4869@gmail.com>
2022-03-05 16:28:12 +0900

    feat: use a part of fontawesome

--- src/client/foundation/side-effects.js
+++ src/client/foundation/side-effects.js
@@ -1,5 +1,7 @@
-import "core-js";
-import "regenerator-runtime/runtime";
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
+import { dom, library } from "@fortawesome/fontawesome-svg-core";
+import { faHandPeace } from "@fortawesome/free-regular-svg-icons";
+import { faInfoCircle, faTicketAlt } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faInfoCircle, faTicketAlt, faHandPeace);
+
+dom.watch();
