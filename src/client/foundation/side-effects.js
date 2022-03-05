85f1a9336c9832f835a94774a928ce38f4ea787e
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-05 18:52:34 +0900

    uralt

--- src/client/foundation/side-effects.js
+++ src/client/foundation/side-effects.js
@@ -1,6 +1,1 @@
-import "core-js";
 import "regenerator-runtime/runtime";
-import "es5-shim";
-import "es6-shim";
-import "es7-shim";
-
--- src/server/index.js
+++ src/server/index.js
@@ -50,7 +50,7 @@ server.register(spaRoute);
 const start = async () => {
   try {
     await initialize();
-    await server.listen(process.env.PORT || 3000, "0.0.0.0");
+    await server.listen(process.env.PORT || 80, "0.0.0.0");
   } catch (err) {
     server.log.error(err);
     process.exit(1);
