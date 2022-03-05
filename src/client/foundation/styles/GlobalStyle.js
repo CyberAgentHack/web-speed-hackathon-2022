23bea42f603365a5c450175940637f34ebda5e69
cathiecode <cathiecode@gmail.com>
2022-03-06 07:32:37 +0900

    add: サブセットフォントを作成して適用

--- src/client/foundation/styles/GlobalStyle.js
+++ src/client/foundation/styles/GlobalStyle.js
@@ -34,6 +34,8 @@ export const GlobalStyle = createGlobalStyle`
     font-family: "Senobi-Gothic";
     font-weight: bold;
     font-display: swap;
-    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.ttf") format("truetype");
+    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold-Subset-Numbers.woff2") format("woff2"),
+    url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold-Subset-Numbers.woff") format("woff"),
+    url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold-Subset-Numbers.ttf") format("truetype");
   }
 `;
