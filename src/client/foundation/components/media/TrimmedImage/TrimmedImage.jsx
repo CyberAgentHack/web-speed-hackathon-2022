0f09bde40aad857050662381cc20be9fb0e7de00
trasta <trasta298@gmail.com>
2022-03-06 03:20:30 +0900

    Revert "Revert "⚡️ TrimmedImageをcssに""
    This reverts commit 731039ac407c35d54a97eb85d5108680f4481e58.

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -1,39 +1,13 @@
-import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
-/**
- * @typedef Props
- * @property {string} src
- * @property {number} width
- * @property {height} height
- */
+export const IconImg = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+`
 
-/** @type {React.VFC<Props>} */
-// TODO: 後で消す(cssでできる)
-export const TrimmedImage = ({ height, src, width }) => {
-  const [dataUrl, setDataUrl] = useState(null);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-
-      const isWidthSmaller = img.width <= img.height;
-      const ratio = isWidthSmaller ? width / img.width : height / img.height;
-
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(
-        img,
-        -(img.width * ratio - width) / 2,
-        -(img.height * ratio - height) / 2,
-        img.width * ratio,
-        img.height * ratio,
-      );
-      setDataUrl(canvas.toDataURL());
-    };
-  }, [height, src, width]);
-
-  return <img loading="lazy" src={dataUrl}/>;
-};
+export const ThmImg = styled.img`
+  width: 400px;
+  height: 225px;
+  object-fit: cover;
+`
