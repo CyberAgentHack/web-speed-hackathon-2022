8e842b1363aa96f7e122e5a993ae43b52893efd9
shellingford330 <pg.econ4869@gmail.com>
2022-03-06 03:20:07 +0900

    feat: remove LayoutShift of heroImage

--- src/client/foundation/pages/Top/internal/HeroImage/HeroImage.jsx
+++ src/client/foundation/pages/Top/internal/HeroImage/HeroImage.jsx
@@ -14,5 +14,5 @@ const Image = styled.img`
 
 /** @type {React.VFC<Props>} */
 export const HeroImage = ({ url }) => {
-  return <Image alt="" src={url} />;
+  return <Image alt="" src={url} style={{ maxWidth: 1024, minHeight: 735 }} />;
 };
