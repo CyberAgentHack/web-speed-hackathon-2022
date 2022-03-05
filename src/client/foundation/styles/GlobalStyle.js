67420edc2db3a68009adfb1f06d921cdf35084f3
tesso57 <tesso1729@gmail.com>
2022-03-06 02:12:05 +0900

    update

--- src/client/foundation/styles/GlobalStyle.js
+++ src/client/foundation/styles/GlobalStyle.js
@@ -27,13 +27,13 @@ export const GlobalStyle = createGlobalStyle`
     font-family: "Senobi-Gothic";
     font-weight: normal;
     font-display: block;
-    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Regular.ttf") format("ttf");
+    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Regular.ttf") format("truetype");
   }
 
   @font-face {
     font-family: "Senobi-Gothic";
     font-weight: bold;
     font-display: block;
-    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.ttf") format("ttf");
+    src: url("/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.ttf") format("truetype");
   }
 `;
