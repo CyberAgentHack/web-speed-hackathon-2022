54efd48134ee5f4a599c9344788e8ce7d1383c18
fujiyamaorange <kaito.bakuman@gmail.com>
2022-03-05 16:26:10 +0900

    use useQuery in useFetch

--- src/client/foundation/hooks/useFetch.js
+++ src/client/foundation/hooks/useFetch.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import {useQuery} from 'react-query'
 
 /**
  * @template T
@@ -15,37 +16,47 @@ import { useEffect, useState } from "react";
  * @returns {ReturnValues<T>}
  */
 export function useFetch(apiPath, fetcher) {
+  const {data, error, isLoading} = useQuery(apiPath, fetcher)
+
   const [result, setResult] = useState({
     data: null,
     error: null,
     loading: true,
   });
 
+  // useEffect(() => {
+  //   setResult(() => ({
+  //     data: null,
+  //     error: null,
+  //     loading: true,
+  //   }));
+
+  //   const promise = fetcher(apiPath);
+
+  //   promise.then((data) => {
+  //     setResult((cur) => ({
+  //       ...cur,
+  //       data,
+  //       loading: false,
+  //     }));
+  //   });
+
+  //   promise.catch((error) => {
+  //     setResult((cur) => ({
+  //       ...cur,
+  //       error,
+  //       loading: false,
+  //     }));
+  //   });
+  // }, [apiPath, fetcher]);
+
   useEffect(() => {
-    setResult(() => ({
-      data: null,
-      error: null,
-      loading: true,
-    }));
-
-    const promise = fetcher(apiPath);
-
-    promise.then((data) => {
-      setResult((cur) => ({
-        ...cur,
-        data,
-        loading: false,
-      }));
-    });
-
-    promise.catch((error) => {
-      setResult((cur) => ({
-        ...cur,
-        error,
-        loading: false,
-      }));
-    });
-  }, [apiPath, fetcher]);
+    setResult({
+      data: data,
+      error: error,
+      loading: isLoading
+    })
+  }, [apiPath, fetcher, data, error, isLoading])
 
   return result;
 }
