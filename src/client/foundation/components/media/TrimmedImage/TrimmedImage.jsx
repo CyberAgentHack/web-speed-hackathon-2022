87942768b1cad07b761237da687b55618a0c1af3
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2022-03-05 23:45:44 +0900

    Fix

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -12,13 +12,9 @@ export const TrimmedImage = ({ height, src, width }) => {
   const [dataUrl, setDataUrl] = useState(null);
 
   useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      const url = src.split("/");
-      url.splice(4, 0, `${width}x${height}`);
-      setDataUrl(url.join("/"));
-    };
+    const url = src.split("/");
+    url.splice(4, 0, `${width}x${height}`);
+    setDataUrl(url.join("/"));
   }, [height, src, width]);
 
   return <img src={dataUrl} loading="lazy" />;
