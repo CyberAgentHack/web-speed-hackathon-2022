1a1fe97e1a5aae98b45f244ab1e710598b26432d
tesso57 <tesso1729@gmail.com>
2022-03-06 03:11:01 +0900

    update

--- other/convert.sh
+++ other/convert.sh
@@ -1,11 +1,10 @@
-ext="jpg"
 con="webp"
 
 # rm files
-for file in ./races/*-*_*.$ext; do rm $file; done
-for file in ./players/*-*_*.$ext; do rm $file; done
+for file in ./races/*; do rm $file; done
+for file in ./players/*; do rm $file; done
 
-# gen files
-for file in ./races/*.$ext; do node convertImage.js $file $con; done
-for file in ./players/*.$ext; do node convertImage.js $file $con; done
+# # gen files
+for file in ./org/races/*.jpg; do node convertImage.js $file $con; done
+for file in ./org/players/*.jpg; do node convertImage.js $file $con; done
 
