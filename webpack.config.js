9551a002a41d89271dde1c822d65e287583a9cba
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-03-05 12:06:31 +0900

    disable inline-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const DIST_PUBLIC = abs("./dist/public");
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = [
   {
-    devtool: "inline-source-map",
+    // devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "client/index.jsx"),
     mode: "development",
     module: {
@@ -64,7 +64,7 @@ module.exports = [
     target: "web",
   },
   {
-    devtool: "inline-source-map",
+    // devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
     mode: "development",
