7b286e810201a1a998944a1c12dc992621bf207e
LilyMameoka <suto.hidetakayellston.com>
2022-03-05 13:06:28 +0900

    :lipstick: 画像サイズ定義

--- src/client/foundation/pages/Top/internal/HeroImage/HeroImage.jsx
+++ src/client/foundation/pages/Top/internal/HeroImage/HeroImage.jsx
@@ -4,6 +4,12 @@ import styled from "styled-components";
 const Image = styled.img`
   display: block;
   margin: 0 auto;
+  width: 1024px;
+  height: 734.57px;
+  @media (max-width: 1024px) {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 /**
@@ -14,5 +20,5 @@ const Image = styled.img`
 
 /** @type {React.VFC<Props>} */
 export const HeroImage = ({ url }) => {
-  return <Image alt="" src={url} />;
+  return <Image alt="" importance="high" src={url} />;
 };
