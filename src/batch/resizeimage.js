4ae7c7b9f0d2ae982b3adb9219e54221f27c4692
onyanko-pon <engineer.tsukasa@gmail.com>
2022-03-05 20:14:08 +0900

    trimmed image output

---
+++ src/batch/resizeimage.js
@@ -0,0 +1,98 @@
+
+// import { resizeImage } from "../client/foundation/utils/ResizeImage";
+const path = require("path");
+const fs = require('fs');
+const PNG = require('png-js');
+const Canvas = require('canvas');
+
+// import { resizeImage } from "../client/foundation/utils/ResizeImage";
+
+const PLAYERS_IMAGE_DIR = path.join(__dirname, "../../public/assets/images/players/");
+const RACE_IMAGE_DIR = path.join(__dirname, "../../public/assets/images/races/");
+
+const RESIZE_PLAYERS_IMAGE_DIR = path.join(__dirname, "../../public/assets/images/resized/players/");
+const RESIZE_RACE_IMAGE_DIR = path.join(__dirname, "../../public/assets/images/resized/races/");
+
+
+// const loadImage = (src) => {
+//   return new Promise((resolve, reject) => {
+//     const img = new Image;
+//     img.crossOrigin = "anonymous";
+//     img.onload = () => resolve(img);
+//     img.onerror = (e) => reject(e);
+//     img.src = src;
+//   });
+// }
+
+const resizeImage = (imageData, width, height) => {
+  const img = new Canvas.Image
+  img.src = imageData
+  // const img = await loadImage(src)
+  img.crossOrigin = "anonymous";
+  const canvas = new Canvas.Canvas;
+  canvas.width = width;
+  canvas.height = height;
+
+  const isWidthSmaller = img.width <= img.height;
+  const ratio = isWidthSmaller ? width / img.width : height / img.height;
+
+  const targetWidth = img.width * ratio
+  const targetHeight = img.height * ratio
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(
+    img,
+    -(img.width * ratio - width) / 2,
+    -(img.height * ratio - height) / 2,
+    img.width * ratio,
+    img.height * ratio,
+  );
+  return canvas.toDataURL("image/png");
+}
+
+
+main()
+
+function jpgToPngSRC(src) {
+  return src.replace(".jpg", `.text`)
+}
+
+function main() {
+  console.log(PLAYERS_IMAGE_DIR)
+  // const imageData = resizeImage(src, width, height)
+  let files = fs.readdirSync(PLAYERS_IMAGE_DIR);
+  const player_width = 100;
+  const player_height = 100;
+  files.forEach(function (file) {
+    const src = PLAYERS_IMAGE_DIR + file
+    const imageData = fs.readFileSync(src)
+    const resizedData = resizeImage(imageData, player_width, player_height)
+
+    const newFileName = `${player_width}_${player_height}_${file}`
+
+    fs.appendFileSync(RESIZE_PLAYERS_IMAGE_DIR + jpgToPngSRC(newFileName), resizedData)
+  });
+
+  files = fs.readdirSync(RACE_IMAGE_DIR);
+  const race_width = 400;
+  const race_height = 225;
+  files.forEach(function (file) {
+    const src = RACE_IMAGE_DIR + file
+    const imageData = fs.readFileSync(src)
+    const resizedData = resizeImage(imageData, race_width, race_height)
+
+    const newFileName = `${race_width}_${race_height}_${file}`
+
+    fs.appendFileSync(RESIZE_RACE_IMAGE_DIR + jpgToPngSRC(newFileName), resizedData)
+  });
+  // for file in files
+  // const data = fs.readFileSync('/Users/joe/test.txt', 'utf8')
+  // fs.readdir(PLAYERS_IMAGE_DIR, function (err, files) {
+  //   if (err) throw err;
+  //   console.log(files)
+  //   // var fileList = files.filter(function (file) {
+  //   //   return fs.statSync(file).isFile() && /.*\.csv$/.test(file); //絞り込み
+  //   // })
+  //   // console.log(fileList);
+  // });
+}
--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -8,8 +8,71 @@ import { getFetchSRCTrimmed } from "../../../utils/Cloudinary";
  * @property {height} height
  */
 
+const PLAYERS_IMAGE_DIR = "/assets/images/players/";
+const RACE_IMAGE_DIR = "/assets/images/races/";
+
+const RESIZE_PLAYERS_IMAGE_DIR = "/assets/images/resized/players/";
+const RESIZE_RACE_IMAGE_DIR = "/assets/images/resized/races/";
+
+const jpgToPngSRC = (src) => {
+  return src.replace(".jpg", `.text`)
+}
+
+const getTrimmedSRC = (src, height, width) => {
+  src = src.replace(PLAYERS_IMAGE_DIR, `${RESIZE_PLAYERS_IMAGE_DIR}${width}_${height}_`)
+  src = src.replace(RACE_IMAGE_DIR, `${RESIZE_RACE_IMAGE_DIR}${width}_${height}_`)
+  return jpgToPngSRC(src)
+}
+
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
-  src = getFetchSRCTrimmed(src, height, width)
-  return <img src={src} />;
+  // src = getFetchSRCTrimmed(src, height, width)
+  const [dataUrl, setDataUrl] = useState(null);
+  const url = getTrimmedSRC(src, height, width)
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        // console.log(data)
+        setDataUrl(data);
+      })
+    // setDataUrl(response.text);
+  }, [height, src, width]);
+  return < img src = { dataUrl } />;
 }
+// export const TrimmedImage = ({ height, src, width }) => {
+//   src = getFetchSRC(src, width)
+//   const [dataUrl, setDataUrl] = useState(null);
+
+//   useEffect(() => {
+//     const img = new Image();
+//     img.src = src;
+//     img.crossOrigin = "anonymous";
+//     img.onload = () => {
+//       const canvas = document.createElement("canvas");
+//       canvas.width = width;
+//       canvas.height = height;
+
+//       const isWidthSmaller = img.width <= img.height;
+//       const ratio = isWidthSmaller ? width / img.width : height / img.height;
+
+//       const targetWidth = img.width * ratio
+//       const targetHeight = img.height * ratio
+
+//       console.log(src, img.width, img.height, targetWidth, targetHeight, width, height)
+
+//       const ctx = canvas.getContext("2d");
+//       ctx.drawImage(
+//         img,
+//         -(img.width * ratio - width) / 2,
+//         -(img.height * ratio - height) / 2,
+//         img.width * ratio,
+//         img.height * ratio,
+//       );
+//       setDataUrl(canvas.toDataURL());
+//     };
+//   }, [height, src, width]);
+
+//   return <img src={dataUrl} />;
+// };
