dea17b7dd287f9ed7e9103220a2b6e4be1267b69
trasta <trasta298@gmail.com>
2022-03-06 06:53:26 +0900

    ⚡️ HTMLのキャッシュ

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -12,6 +12,12 @@ export const spaRoute = async (fastify) => {
     lastModified: true,
     maxAge: 604800 * 1000,
     root: join(__dirname, "public"),
+    setHeaders: (res, path) => {
+      // Custom Cache-Control for HTML files
+      if (path.endsWith(".html")) {
+        res.header("Cache-Control", "public, max-age=300, s-maxage=180");
+      }
+    },
     wildcard: false,
   });
 
