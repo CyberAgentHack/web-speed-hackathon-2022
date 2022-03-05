c616aa277cce30efd9b4076afad5cb92ef0593db
cathiecode <cathiecode@gmail.com>
2022-03-06 04:23:34 +0900

    change: TrimmedImageの実装をsvgに変更

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useMemo, memo } from "react";
 import styled from "styled-components";
 
-const ImageAutoHeight = styled.img`
+const WrapperSvg = styled.svg`
+  display: block;
   height: auto;
+  max-width: 100%;
 `
 
 /**
@@ -14,7 +16,8 @@ const ImageAutoHeight = styled.img`
 
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = memo(({ height, src, width }) => {
-  const [dataUrl, setDataUrl] = useState(null);
+  const [position, setPosition] = useState({x: 0, y: 0, width: 0, height: 0});
+
 
   useEffect(() => {
     if (src === undefined) {
@@ -23,24 +26,33 @@ export const TrimmedImage = memo(({ height, src, width }) => {
     const img = new Image();
     img.src = src;
     img.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-
       const isWidthSmaller = img.width <= img.height;
       const ratio = isWidthSmaller ? width / img.width : height / img.height;
 
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(
-        img,
-        -(img.width * ratio - width) / 2,
-        -(img.height * ratio - height) / 2,
-        img.width * ratio,
-        img.height * ratio,
-      );
-      setDataUrl(canvas.toDataURL());
+      setPosition({
+        x: Math.round(-(img.width * ratio - width) / 2),
+        y: Math.round(-(img.height * ratio - height) / 2),
+        width: Math.round(img.width * ratio),
+        height: Math.round(img.height * ratio),
+      });
     };
   }, [src, height, width]);
 
-  return <ImageAutoHeight src={dataUrl} alt="" width={width} height={height} />;
+  return (
+    <WrapperSvg width={width} height={height} viewBox={`0 0 ${width} ${height}`} preserveAspectRatio="none">
+      <image x={position.x} y={position.y} width={position.width} height={position.height} href={src} />
+    </WrapperSvg>
+  )
+  /*return (
+    <Wrapper $width={width} $height={height}>
+      <PositionedImage
+        src={src}
+        alt=""
+        $width={position.width}
+        $height={position.height}
+        $x={position.x}
+        $y={position.y}
+      />
+    </Wrapper>
+  )*/
 });
