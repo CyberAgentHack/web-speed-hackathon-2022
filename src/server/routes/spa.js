413ec0db5f227fa10c92b5c679a9f6581f250797
onyanko-pon <engineer.tsukasa@gmail.com>
2022-03-05 15:38:54 +0900

    fasify gzip

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -6,8 +6,11 @@ import fastifyStatic from "fastify-static";
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const spaRoute = async (fastify) => {
+  // fastify.register(require('fastify-compress'));
+
   fastify.register(fastifyStatic, {
     root: join(__dirname, "public"),
+    prefix: '/assets/',
     wildcard: false,
   });
 
@@ -17,5 +20,11 @@ export const spaRoute = async (fastify) => {
 
   fastify.get("*", (_req, reply) => {
     return reply.sendFile("index.html", join(__dirname, "public"));
+  });
+
+  fastify.get("/main.js", async (_req, reply) => {
+    // reply.header("Cache-Control", "1000");
+    reply.header("Content-Encoding", "gzip");
+    return reply.sendFile("main.js.gz", join(__dirname, "public"));
   });
 };
