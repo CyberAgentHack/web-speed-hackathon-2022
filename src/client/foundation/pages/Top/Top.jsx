c008ff5a146a2ec2892dbb6220af113d14fc5651
jam411 <r.j.shimizu@jaist.ac.jp>
2022-03-05 12:19:03 +0900

    test change

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -167,7 +167,7 @@ export const Top = () => {
 
       <Spacer mt={Space * 2} />
       <section>
-        <Heading as="h1">本日のレース</Heading>
+        <Heading as="h1">本日のレース Today's Race</Heading>
         {todayRacesToShow.length > 0 && (
           <RecentRaceList>
             {todayRacesToShow.map((race) => (
