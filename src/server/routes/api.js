d2f276383ec2a080f11ee910db4ff4c999c7841a
jam411 <r.j.shimizu@jaist.ac.jp>
2022-03-05 20:02:25 +0900

    api.js gzip

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -10,6 +10,12 @@ import { initialize } from "../typeorm/initialize.js";
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const apiRoute = async (fastify) => {
+
+  fastify.get('/*.js', (req, res) => {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+  });
+
   fastify.get("/users/me", async (req, res) => {
     const repo = (await createConnection()).getRepository(User);
 
