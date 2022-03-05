8882170d3191b8d9b87f0867c4778705857296fd
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2022-03-05 19:06:32 +0900

    change into prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -76,7 +76,7 @@ module.exports = [
     devtool: false,
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
-    mode: "production",
+    mode: "development",
     module: {
       rules: [
         {
