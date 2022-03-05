85b4aabe4b2a9b300476b05c8497e093cc2e7970
trasta <trasta298@gmail.com>
2022-03-05 18:46:39 +0900

    🔥 remove moment from dateutil

--- src/client/foundation/utils/DateUtils.js
+++ src/client/foundation/utils/DateUtils.js
@@ -1,11 +1,15 @@
-import moment from "moment-timezone";
 /**
  * @param {string} dateLeft
  * @param {string} dateRight
  * @returns {boolean}
  */
 export const isSameDay = (dateLeft, dateRight) => {
-  return moment(dateLeft).isSame(moment(dateRight), "day");
+  const left = new Date(dateLeft);
+  const right = new Date(dateRight);
+  return left.getFullYear() === right.getFullYear() &&
+         left.getMonth() === right.getMonth() &&
+         left.getDate() === right.getDate();
+  // return moment(dateLeft).isSame(moment(dateRight), "day");
 };
 
 /**
@@ -14,7 +18,10 @@ export const isSameDay = (dateLeft, dateRight) => {
  * @returns {string}
  */
 export const formatTime = (ts) => {
-  return moment(ts).format("H:mm");
+  // 分は 二桁
+  const d = new Date(ts);
+  return `${d.getHours()}:${d.getMinutes().toString().padStart(2, '0')}`;
+  // return moment(ts).format("H:mm");
 };
 
 /**
@@ -23,13 +30,21 @@ export const formatTime = (ts) => {
  * @returns {string}
  */
 export const formatCloseAt = (closeAt, now = new Date()) => {
-  if (moment(closeAt).isBefore(now)) {
+  // 同じ時間は flase
+  // if (moment(closeAt).isBefore(now)) {
+  //   return "投票締切";
+  // }
+  if (Date.parse(closeAt) < now) {
     return "投票締切";
   }
 
-  if (moment(closeAt).isAfter(moment(now).add(2, "hours"))) {
+  // if (moment(closeAt).isAfter(moment(now).add(2, "hours"))) {
+  //   return "投票受付中";
+  // }
+  if (Date.parse(closeAt) > now.getTime() + 2 * 60 * 60 * 1000) {
     return "投票受付中";
   }
 
-  return `締切${moment(closeAt).diff(now, "minutes")}分前`;
+  // return `締切${moment(closeAt).diff(now, "minutes")}分前`;
+  return `締切${Math.floor((Date.parse(closeAt) - now.getTime()) / 1000 / 60)}分前`;
 };
