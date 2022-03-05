fb4df09b6da2fd9a232793a698beaad71713a3a0
progriro <progriro@gmail.com>
2022-03-05 20:02:53 +0900

    use object-fit: cover

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const CoverImage = styled.img`
+  object-fit: cover;
+`;
 
 /**
  * @typedef Props
  * @property {string} src
  * @property {number} width
- * @property {height} height
+ * @property {number} height
  */
 
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
-  const [dataUrl, setDataUrl] = useState(null);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-
-      const isWidthSmaller = img.width <= img.height;
-      const ratio = isWidthSmaller ? width / img.width : height / img.height;
-
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(
-        img,
-        -(img.width * ratio - width) / 2,
-        -(img.height * ratio - height) / 2,
-        img.width * ratio,
-        img.height * ratio,
-      );
-      setDataUrl(canvas.toDataURL());
-    };
-  }, [height, src, width]);
-
-  return <img src={dataUrl} />;
+  return <CoverImage height={height} loading="lazy" src={src} width={width} />;
 };
