90bafa224b595909bee1b8bcc3939db6ab95f269
cathiecode <cathiecode@gmail.com>
2022-03-05 17:30:31 +0900

    fix: アイコン違った

--- src/client/foundation/pages/races/RaceResult/internal/RaceResultSection/RaceResultSection.jsx
+++ src/client/foundation/pages/races/RaceResult/internal/RaceResultSection/RaceResultSection.jsx
@@ -1,4 +1,4 @@
-import { faHandPeace } from "@fortawesome/free-solid-svg-icons";
+import { faHandPeace } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
