13423ee89c43215b7820644349b58716d13e7708
Re-taro <aminokacho@gmail.com>
2022-03-05 22:24:26 +0900

    ✨ cache data

--- src/server/index.js
+++ src/server/index.js
@@ -40,8 +40,8 @@ server.addHook("onRequest", async (req, res) => {
 });
 
 server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "no-cache, no-store, no-transform");
-  res.header("Connection", "close");
+  res.header("Cache-Control", "public, max-age=86400");
+  res.header("Connection", "Keep-Alive");
 });
 
 server.register(apiRoute, { prefix: "/api" });
