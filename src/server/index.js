0ede7b9c9291f68526ed35449e52a96363e5f7e7
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-05 18:16:51 +0900

    cache

--- src/server/index.js
+++ src/server/index.js
@@ -38,8 +38,8 @@ server.addHook("onRequest", async (req, res) => {
 });
 
 server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "no-cache, no-store, no-transform");
-  res.header("Connection", "close");
+  res.header("Cache-Control", "public, max-age=31536000");
+  res.header("Connection", "Keep-Alive");
 });
 
 server.register(apiRoute, { prefix: "/api" });
