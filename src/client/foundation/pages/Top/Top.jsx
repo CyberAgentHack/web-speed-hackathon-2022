b40457a46b09bfa30fccf5c66c4ac94edd1ae7eb
trasta <trasta298@gmail.com>
2022-03-05 13:22:01 +0900

    🔥 remove lodash func

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import moment from "moment-timezone";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -30,11 +29,9 @@ function useTodayRacesWithAnimation(races) {
   const timer = useRef(null);
 
   useEffect(() => {
-    const isRacesUpdate =
-      _.difference(
-        races.map((e) => e.id),
-        prevRaces.current.map((e) => e.id),
-      ).length !== 0;
+
+    const d = prevRaces.current.map((e) => e.id);
+    const isRacesUpdate = races.map((e) => e.id).filter(i => d.indexOf(i) === -1).length !== 0;
 
     prevRaces.current = races;
     setIsRacesUpdate(isRacesUpdate);
@@ -62,7 +59,7 @@ function useTodayRacesWithAnimation(races) {
       }
 
       numberOfRacesToShow.current++;
-      setRacesToShow(_.slice(races, 0, numberOfRacesToShow.current));
+      setRacesToShow(races.slice(0, numberOfRacesToShow.current));
     }, 100);
   }, [isRacesUpdate, races]);
 
