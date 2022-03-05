37c7e033499f7a0c07efaed1766ff5d810279d10
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-05 17:55:00 +0900

    fix webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 const path = require("path");
 
 const CopyPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const nodeExternals = require("webpack-node-externals");
 
 function abs(...args) {
@@ -71,6 +73,7 @@ module.exports = [
       new CopyPlugin({
         patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
       }),
+      new BundleAnalyzerPlugin(),
     ],
     resolve: {
       extensions: [".js", ".jsx"],
