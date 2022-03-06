20b8b16046e63e9360d21eafd4226e3a9c0b9371
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-06 13:17:27 +0900

    Revert "switch preact"
    This reverts commit df29020f85bac9bbe2edc360d88532ccc3a8b8f1.

--- webpack.config.js
+++ webpack.config.js
@@ -76,10 +76,6 @@ module.exports = [
       // new BundleAnalyzerPlugin(),
     ],
     resolve: {
-      alias: {
-        react: "preact/compat",
-        "react-dom": "preact/compat",
-      },
       extensions: [".js", ".jsx"],
     },
     target: "web",
