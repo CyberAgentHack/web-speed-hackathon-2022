3504737c1ac3ef74d92cf23fb199e83014784b51
onyanko-pon <engineer.tsukasa@gmail.com>
2022-03-06 17:23:19 +0900

    remove solid font

--- src/client/foundation/side-effects.js
+++ src/client/foundation/side-effects.js
@@ -4,5 +4,5 @@ import "es5-shim";
 import "es6-shim";
 import "es7-shim";
 import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
+// import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
