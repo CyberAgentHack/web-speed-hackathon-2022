b22c621b1a49434031a5d2cc5620b8dff3d98f9d
Re-taro <aminokacho@gmail.com>
2022-03-06 13:58:16 +0900

    ⚡️ lazy

--- src/client/foundation/routes.jsx
+++ src/client/foundation/routes.jsx
@@ -1,16 +1,16 @@
-import React, { lazy, Suspense } from "react";
+import React, { Suspense } from "react";
 import { Route, Routes as RouterRoutes } from "react-router-dom";
 
-const CommonLayout = lazy(() => import("./layouts/CommonLayout"));
-const Top = lazy(() => import("./pages/Top"));
-const Odds = lazy(() => import("./pages/races/Odds"));
-const RaceCard = lazy(() => import("./pages/races/RaceCard"));
-const RaceResult = lazy(() => import("./pages/races/RaceResult"));
+import { CommonLayout } from "./layouts/CommonLayout";
+const Top = React.lazy(() => import("./pages/Top"));
+const Odds = React.lazy(() => import("./pages/races/Odds"));
+const RaceCard = React.lazy(() => import("./pages/races/RaceCard"));
+const RaceResult = React.lazy(() => import("./pages/races/RaceResult"));
 
 /** @type {React.VFC} */
 export const Routes = () => {
   return (
-    <Suspense fallback={<></>}>
+    <Suspense fallback={null}>
       <RouterRoutes>
         <Route element={<CommonLayout />} path="/">
           <Route index element={<Top />} />
