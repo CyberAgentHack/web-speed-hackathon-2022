f31e1058a9a52efe94b2514dd25869615aee5174
shoya ijichi <e1q19006@oit.ac.jp>
2022-03-06 16:30:32 +0900

    OK>

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -1,4 +1,5 @@
-import _ from "lodash-es";
+import _ from "lodash";
+import moment from "moment-timezone";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
@@ -8,11 +9,11 @@ import { Spacer } from "../../components/layouts/Spacer";
 import { Stack } from "../../components/layouts/Stack";
 import { Heading } from "../../components/typographies/Heading";
 import { useAuthorizedFetch } from "../../hooks/useAuthorizedFetch";
-// import { useFetch } from "../../hooks/useFetch";
+import { useFetch } from "../../hooks/useFetch";
 import { Color, Radius, Space } from "../../styles/variables";
 import { isSameDay } from "../../utils/DateUtils";
 import { authorizedJsonFetcher, jsonFetcher } from "../../utils/HttpUtils";
-import useSWR from 'swr'
+
 import { ChargeDialog } from "./internal/ChargeDialog";
 import { HeroImage } from "./internal/HeroImage";
 import { RecentRaceList } from "./internal/RecentRaceList";
@@ -86,7 +87,7 @@ function useHeroImage(todayRaces) {
     firstRaceId !== undefined
       ? `/api/hero?firstRaceId=${firstRaceId}`
       : "/api/hero";
-  const { data } = useSWR(url, jsonFetcher);
+  const { data } = useFetch(url, jsonFetcher);
 
   if (firstRaceId === undefined || data === null) {
     return null;
@@ -96,21 +97,15 @@ function useHeroImage(todayRaces) {
   return imageUrl;
 }
 
-const getDate = () => {
-  const t=new Date()
-  return t.getFullYear() + "-" + (t.getMonth() + 1) + "-" + t.getDate()
-}
-
 /** @type {React.VFC} */
 export const Top = () => {
-  const { date = getDate() } = useParams();
+  const { date = moment().format("YYYY-MM-DD") } = useParams();
 
   const ChargeButton = styled.button`
     background: ${Color.mono[700]};
     border-radius: ${Radius.MEDIUM};
     color: ${Color.mono[0]};
     padding: ${Space * 1}px ${Space * 2}px;
-
     &:hover {
       background: ${Color.mono[800]};
     }
@@ -123,7 +118,7 @@ export const Top = () => {
     authorizedJsonFetcher,
   );
 
-  const { data: raceData } = useSWR("/api/races", jsonFetcher);
+  const { data: raceData } = useFetch("/api/races", jsonFetcher);
 
   const handleClickChargeButton = useCallback(() => {
     if (chargeDialogRef.current === null) {
@@ -142,18 +137,18 @@ export const Top = () => {
       ? [...raceData.races]
           .sort(
             (/** @type {Model.Race} */ a, /** @type {Model.Race} */ b) =>
-              new Date(a.startAt).getTime()-new Date(b.startAt).getTime()
+              moment(a.startAt) - moment(b.startAt),
           )
           .filter((/** @type {Model.Race} */ race) =>
             isSameDay(race.startAt, date),
           )
       : [];
   const todayRacesToShow = useTodayRacesWithAnimation(todayRaces);
-  // const heroImageUrl = useHeroImage(todayRaces);
+  const heroImageUrl = useHeroImage(todayRaces);
 
   return (
     <Container>
-      <HeroImage url="/assets/images/hero.jpg?6666666666" />
+      {heroImageUrl !== null && <HeroImage url={heroImageUrl} />}
 
       <Spacer mt={Space * 2} />
       {userData && (
--- src/client/foundation/utils/DateUtils.js
+++ src/client/foundation/utils/DateUtils.js
@@ -1,12 +1,11 @@
+import moment from "moment-timezone";
 /**
  * @param {string} dateLeft
  * @param {string} dateRight
  * @returns {boolean}
  */
 export const isSameDay = (dateLeft, dateRight) => {
-  const left = new Date(dateLeft);
-  const right = new Date(dateRight);
-  return "a"+left.getFullYear()+left.getMonth()+left.getDay() === "a"+right.getFullYear()+right.getMonth()+right.getDay()
+  return moment(dateLeft).isSame(moment(dateRight), "day");
 };
 
 /**
@@ -15,9 +14,7 @@ export const isSameDay = (dateLeft, dateRight) => {
  * @returns {string}
  */
 export const formatTime = (ts) => {
-  console.log(ts)
-  const t=new Date(ts)
-  return t.getHours()+":"+t.getMinutes()
+  return moment(ts).format("H:mm");
 };
 
 /**
@@ -26,16 +23,13 @@ export const formatTime = (ts) => {
  * @returns {string}
  */
 export const formatCloseAt = (closeAt, now = new Date()) => {
-  const closeAtDate = new Date(closeAt);
-  if (now.getTime() > closeAtDate.getTime()) {
+  if (moment(closeAt).isBefore(now)) {
     return "投票締切";
   }
 
-  // if (moment(closeAt).isAfter(moment(now).add(2, "hours"))) {
-  if (now.getTime()+2*60*60*1000 < closeAtDate.getTime()) {
+  if (moment(closeAt).isAfter(moment(now).add(2, "hours"))) {
     return "投票受付中";
   }
 
-  // return `締切${moment(closeAt).diff(now, "minutes")}分前`;
-  return `締切${Math.trunc((closeAtDate.getTime()-now.getTime())/1000/60)}分前`;
+  return `締切${moment(closeAt).diff(now, "minutes")}分前`;
 };
