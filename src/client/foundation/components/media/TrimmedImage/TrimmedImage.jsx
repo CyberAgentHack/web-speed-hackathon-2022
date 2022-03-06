807895b84034218904376fd71e51c2b2bb449c15
Daiki Usami <daiusa3@icloud.com>
2022-03-06 10:08:13 +0900

    fix trim

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -8,21 +8,17 @@ import styled from "styled-components";
  * @property {height} height
  */
 
+const ContainImage = styled.img`
+  height: 100%;
+  object-fit: "contain";`
+const CoverImage = styled.img`
+  object-fit: "cover";`
+
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
-  const StyledImage = styled.img`
-    height: 100%;
-    aspect-ratio: ${width} / ${height};
-    object-fit: "contain";`
-
-  return <StyledImage loading="lazy" src={src} />;
+  return <ContainImage aspect-ratio={width / height} loading="lazy" src={src} />;
 };
 
 export const FixedImage = ({ height, src, width }) => {
-  const StyledImage = styled.img`
-    height: "${height}px";
-    width: "${width}px";
-    object-fit: "cover";`
-
-  return <StyledImage loading="lazy" src={src} />;
+  return <CoverImage height={height} loading="lazy" src={src} width={width} />;
 };
