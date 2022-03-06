e1cb64b6bfe867a1381b2a3cf17d0f6ef193b2d9
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-03-06 11:18:41 +0900

    fix mode from develop to production

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = [
   {
     // devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "client/index.jsx"),
-    mode: "development",
+    mode: "production",
     module: {
       rules: [
         {
@@ -73,7 +73,7 @@ module.exports = [
     // devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
-    mode: "development",
+    mode: "production",
     module: {
       rules: [
         {
