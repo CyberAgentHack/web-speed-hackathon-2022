4a3f378d692377cb0bedbeed50470b8a6aedfff3
progriro <progriro@gmail.com>
2022-03-06 09:53:04 +0900

    enable cache on server

--- src/server/index.js
+++ src/server/index.js
@@ -30,9 +30,8 @@ server.addHook("onRequest", async (req, res) => {
   }
 });
 
-server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "no-cache, no-store, no-transform");
-  res.header("Connection", "close");
+server.addHook("onRequest", async (_req, res) => {
+  res.header("Cache-Control", "public, max-age=604800, immutable");
 });
 
 server.register(apiRoute, { prefix: "/api" });
