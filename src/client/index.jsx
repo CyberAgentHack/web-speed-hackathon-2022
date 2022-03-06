0c7aad575d88d49d1e559d2ba48a33cf31aad49b
progriro <progriro@gmail.com>
2022-03-06 09:18:19 +0900

    defer fetch font

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,3 +5,10 @@ import { App } from "./foundation/App";
 
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
+
+requestIdleCallback(() => {
+  const $font = document.createElement("link");
+  $font.rel = "stylesheet";
+  $font.href = "/styles/foundation/font.css";
+  document.head.append($font);
+});
