24ba4af496cdb391dbf08cf3b53d20310c109ec5
hijiki51 <hibiki0719euph@gmail.com>
2022-03-06 13:09:42 +0900

    ⚡  use dayjs

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -1,4 +1,4 @@
-import moment from "moment-timezone";
+import dayjs from "dayjs";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
@@ -94,7 +94,7 @@ function useHeroImage(todayRaces) {
 
 /** @type {React.VFC} */
 export const Top = () => {
-  const { date = moment().format("YYYY-MM-DD") } = useParams();
+  const { date = dayjs().format("YYYY-MM-DD") } = useParams();
 
   const ChargeButton = styled.button`
     background: ${Color.mono[700]};
@@ -133,7 +133,7 @@ export const Top = () => {
       ? [...raceData.races]
         .sort(
           (/** @type {Model.Race} */ a, /** @type {Model.Race} */ b) =>
-            moment(a.startAt) - moment(b.startAt),
+            dayjs(a.startAt) - dayjs(b.startAt),
         )
         .filter((/** @type {Model.Race} */ race) =>
           isSameDay(race.startAt, date),
