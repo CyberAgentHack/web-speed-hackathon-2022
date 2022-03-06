808d340d8133dae605ddcf2b4e92a2a9f377f64a
cathiecode <cathiecode@gmail.com>
2022-03-06 16:47:01 +0900

    fix: lazy loading やめた

--- src/client/foundation/routes.jsx
+++ src/client/foundation/routes.jsx
@@ -2,10 +2,11 @@ import React, { Suspense } from "react";
 import { Route, Routes as RouterRoutes } from "react-router-dom";
 
 import { CommonLayout } from "./layouts/CommonLayout";
-const Top = React.lazy(() => import("./pages/Top"));
-const Odds = React.lazy(() => import("./pages/races/Odds"));
-const RaceCard = React.lazy(() => import("./pages/races/RaceCard"));
-const RaceResult = React.lazy(() => import("./pages/races/RaceResult"));
+
+import Top from "./pages/Top"
+import Odds from "./pages/races/Odds"
+import RaceCard from "./pages/races/RaceCard"
+import RaceResult from "./pages/races/RaceResult"
 
 /** @type {React.VFC} */
 export const Routes = () => {
