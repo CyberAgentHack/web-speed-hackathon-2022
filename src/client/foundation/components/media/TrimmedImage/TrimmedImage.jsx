d6ecb633b33f1a0edab78c926a6771a90c9872af
LilyMameoka <suto.hidetakayellston.com>
2022-03-06 10:30:04 +0900

    :lipstick: 画像のサイズ定義

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -34,5 +34,5 @@ export const TrimmedImage = ({ height, src, width }) => {
     };
   }, [height, src, width]);
 
-  return <img alt="" src={dataUrl} />;
+  return <img alt="" decoding='async' loading='async' src={dataUrl} style={{height: height+'px', width: width+'px'}} />;
 };
