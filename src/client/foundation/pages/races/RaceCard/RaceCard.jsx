8b0741e04229c621ac17f53509f20a4eb16e3b81
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-03-06 13:36:39 +0900

    fix image from jpg to webp

--- src/client/foundation/pages/races/RaceCard/RaceCard.jsx
+++ src/client/foundation/pages/races/RaceCard/RaceCard.jsx
@@ -67,7 +67,7 @@ export const RaceCard = () => {
           {data.entries.map((entry) => (
             <PlayerPictureList.Item
               key={entry.id}
-              image={entry.player.image}
+              image={`{entry.player.image}.webp`}
               name={entry.player.name}
               number={entry.number}
             />
