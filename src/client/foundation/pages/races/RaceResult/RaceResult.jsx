ae946fc0af9df6cff9fce213ece700a3532df781
Daiki Usami <daiusa3@icloud.com>
2022-03-06 14:20:14 +0900

    fixed trim

--- src/client/foundation/pages/races/RaceResult/RaceResult.jsx
+++ src/client/foundation/pages/races/RaceResult/RaceResult.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { Container } from "../../../components/layouts/Container";
 import { Section } from "../../../components/layouts/Section";
 import { Spacer } from "../../../components/layouts/Spacer";
-import { TrimmedImage } from "../../../components/media/TrimmedImage";
+import { FixedImage } from "../../../components/media/TrimmedImage";
 import { TabNav } from "../../../components/navs/TabNav";
 import { Heading } from "../../../components/typographies/Heading";
 import { useAuthorizedFetch } from "../../../hooks/useAuthorizedFetch";
@@ -53,7 +53,7 @@ export const RaceResult = () => {
       <Section dark shrink>
         <LiveBadge>Live</LiveBadge>
         <Spacer mt={Space * 2} />
-        <TrimmedImage height={225} src={jpg2avif(data.image)} width={400}/>
+        <FixedImage height={225} src={jpg2avif(data.image)} width={400}/>
       </Section>
 
       <Spacer mt={Space * 2} />
