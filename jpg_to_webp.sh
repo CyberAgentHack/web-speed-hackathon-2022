d5a68c816e15de5d60af751addc2266df0b74386
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2022-03-06 13:48:49 +0900

    add jpg_to_webp.jp

---
+++ jpg_to_webp.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+echo "=============jpg To webp==============="
+
+app_root_path=`pwd`
+assets_path="${app_root_path}/public/assets/images/races/"
+
+cd $assets_path
+
+for file in *.jpg
+do
+  cwebp -q 50 "$file" -o "${file%.*}.webp"
+done
+
+cd $app_root_path
+
+echo "===========Excuted!============"
