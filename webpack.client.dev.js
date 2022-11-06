b5a3eb0678713f354f1838e89ad0e544826001ed
TsuyoshiNISHIKAWA <cony.tn@gmail.com>
2022-11-06 16:11:01 +0900

    remove webpack-bundle-analyzer

--- webpack.client.dev.js
+++ webpack.client.dev.js
@@ -1,12 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require("webpack-merge");
 
 const common = require("./webpack.client.common.js");
 
 module.exports = merge(common, {
   mode: "development",
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
 });
