c96fb2294aba565e0e8776b9a772d574c06b8e80
TsuyoshiNISHIKAWA <cony.tn@gmail.com>
2022-11-07 07:32:15 +0900

    Prodでinline-source-mapを無効化

--- webpack.client.common.js
+++ webpack.client.common.js
@@ -13,7 +13,6 @@ const DIST_PUBLIC = abs("./dist/public");
 
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = {
-  devtool: "inline-source-map",
   entry: path.join(SRC_ROOT, "client/index.jsx"),
   mode: "development",
   module: {
--- webpack.client.dev.js
+++ webpack.client.dev.js
@@ -4,5 +4,6 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.client.common.js");
 
 module.exports = merge(common, {
+  devtool: "inline-source-map",
   mode: "development",
 });
