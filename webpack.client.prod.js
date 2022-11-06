a9382c994e779932414ef25b63392f254ee84740
TsuyoshiNISHIKAWA <cony.tn@gmail.com>
2022-11-06 19:12:00 +0900

    prodビルドでconsoleを消す

--- webpack.client.prod.js
+++ webpack.client.prod.js
@@ -1,8 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge");
 
 const common = require("./webpack.client.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    })],
+  },
 });
