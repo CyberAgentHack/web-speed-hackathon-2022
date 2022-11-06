e961732a89c1af02e352497e89da3c116cbe3013
nakamitsu_nozomi <exnnss0131@gmail.com>
2022-11-06 18:48:36 +0900

    fix: style崩れ

--- src/client/foundation/pages/Top/internal/HeroImage/HeroImage.jsx
+++ src/client/foundation/pages/Top/internal/HeroImage/HeroImage.jsx
@@ -4,26 +4,14 @@ import styled from "styled-components";
 /** @type {React.VFC<Props>} */
 export const HeroImage = ({ url }) => {
   return (
-    <Wrapper>
-      <Img alt="" loading="lazy" src={url} />
-    </Wrapper>
+    <>
+      <Img alt="" height="100" loading="lazy" src={url} width="200" />
+    </>
   );
 };
 
-const Wrapper = styled.div`
-  position: relative;
-  &:before {
-    content: "";
-    display: block;
-    padding-top: 71.6796%;
-  }
-`;
 const Img = styled.img`
-  display: block;
-  margin: 0 auto;
-  position: absolute;
-  top: 0;
-  left: 0;
   width: 100%;
-  height: auto;
+  height: 100%;
+  object-fit: cover;
 `;
