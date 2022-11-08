0e5bf72351de39ef24db048e7464226e357096f6
TsuyoshiNISHIKAWA <cony.tn@gmail.com>
2022-11-08 14:08:15 +0900

    IE対応用のライブラリを削除

--- src/client/foundation/side-effects.js
+++ src/client/foundation/side-effects.js
@@ -1,5 +1,3 @@
-import "core-js";
-import "regenerator-runtime/runtime";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHandPeace } from "@fortawesome/free-regular-svg-icons";
 import { faCircleInfo, faTicket } from "@fortawesome/free-solid-svg-icons";
