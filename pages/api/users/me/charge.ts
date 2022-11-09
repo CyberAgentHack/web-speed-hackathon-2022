a119f8781a6ba866aae9142317be159b4a58df5c
tsuyoshi-nishikawa <tsuyoshi-nishikawa@example.com>
2022-11-09 17:09:50 +0900

    とりあえずサンプルが起動するまで

---
+++ pages/api/users/me/charge.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+    res.send("charge");
+}
