51d2def9a1f395edd7f2fba5261f9d95b9f6c311
“halmuda” <halmuda6231@gmail.com>
2022-11-11 00:11:25 +0900

    update: webpackのsource-map削除

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ const DIST_PUBLIC = abs("./dist/public");
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = [
   {
-    devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "client/index.jsx"),
     mode: "production",
     module: {
@@ -65,7 +64,6 @@ module.exports = [
     target: "web",
   },
   {
-    devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
     mode: "production",
