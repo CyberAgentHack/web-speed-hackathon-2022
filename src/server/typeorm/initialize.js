3a3ba10a0eb705e2983a41685ebe4ab59840390c
TsuyoshiNISHIKAWA <cony.tn@gmail.com>
2022-11-13 09:26:14 +0900

    DBインデックス追加

--- src/server/typeorm/initialize.js
+++ src/server/typeorm/initialize.js
@@ -1,7 +1,15 @@
 import fs from "fs/promises";
+import sqlite3 from "sqlite3";
 
 import { DATABASE_PATH, INITIAL_DATABASE_PATH } from "../paths";
 
 export async function initialize() {
   await fs.copyFile(INITIAL_DATABASE_PATH, DATABASE_PATH);
+
+  const db = new sqlite3.Database(DATABASE_PATH);
+  db.run("create index IDX_RACE_ENTRY_PLAYER_ID on race_entry (playerId)");
+  db.run("create index IDX_RACE_ENTRY_RACE_ID on race_entry (raceId)");
+  db.run("create index IDX_ODDS_ITEM_RACE_ID on odds_item (raceId)");
+  db.run("create index IDX_BETTING_TICKET_RACE_ID on betting_ticket (raceId)");
+  db.run("create index IDX_BETTING_TICKET_USER_ID on betting_ticket (userId)");
 }
