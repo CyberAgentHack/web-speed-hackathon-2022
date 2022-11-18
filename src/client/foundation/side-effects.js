6828b68301307d9022553b00db5e65ef4e28d529
“halmuda” <halmuda6231@gmail.com>
2022-11-18 23:12:10 +0900

    del: side-effects.js

--- src/client/foundation/side-effects.js
+++
@@ -1,8 +0,0 @@
-import "core-js";
-import "regenerator-runtime/runtime";
-import "es5-shim";
-import "es6-shim";
-import "es7-shim";
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
